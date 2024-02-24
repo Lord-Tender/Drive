229ece9aaa698cae6f39415f9e7c4062c4252cdb
Emmanuel <emmanuelola961@gmail.com>
2024-02-24 11:04:57 +0100

    Done with all page and dashboard interface

---
+++ public/script.js
@@ -0,0 +1,9 @@
+const showFile = () =>{
+    document.getElementById('myFile').style.display = 'block'
+    document.getElementById('uploadFile').style.display = 'none'
+}
+
+const showUpload = () =>{
+    document.getElementById('uploadFile').style.display = 'block'
+    document.getElementById('myFile').style.display = 'none'
+}
